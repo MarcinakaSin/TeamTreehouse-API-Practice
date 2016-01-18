b5e6839093bb09163e5a63d634fe51dc5355fc4f
MarcinakaSin <marcinmu330@gmail.com>
2016-01-18 17:50:17 -0600

    keep forgetting to remove this

--- js/services.js
+++ js/services.js
@@ -4,7 +4,7 @@ var services = angular.module('movie.services', []);
 
 services.factory('movies', function( $http ){
 
-	var apiKey = 'ac5d3d00a8b1abfd5439fad20e0cb342';
+	var apiKey = 'API KEY GOES HERE';
 
 	function load(path, params) {
 		return $http.jsonp('https://api.themoviedb.org/3/movie/' + path + '?api_key=' + apiKey + '&append_to_response=images,releases,trailers&language=en&include_image_language=en,null&callback=JSON_CALLBACK');
