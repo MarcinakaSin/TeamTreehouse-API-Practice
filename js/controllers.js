128dea4f573d58c170462c3def2506e6474412df
MarcinakaSin <marcinmu330@gmail.com>
2016-01-19 02:12:05 -0600

    Finished version

--- js/controllers.js
+++ js/controllers.js
@@ -6,11 +6,28 @@ var controllers = angular.module('movies.controllers', []);
 
 controllers.controller('MovieListCtrl', function ($scope, movies, $routeParams) {
 
-	var list = $routeParams.list;
-	movies.list(list).then(function (data){
-		$scope.list = data.data;
-		console.log(data);
-	});
+	var validLists = ['now_playing', 'popular', 'top_rated', 'upcoming'];
+	var list = $routeParams.list.toLowerCase();
+
+	if(validLists.indexOf(list) > -1) {
+		movies.list(list).then(function (data){
+			$scope.list = data.data;
+			console.log(data);
+		});
+	} else { 
+		window.location.href = '#/now_playing';
+	}
+
+	$scope.loadNextPage = function() {
+		movies.list(list, { page: $scope.list.page + 1 }).then(function(data) {
+			//	if we wanted to replace the data on the page instead of concatinating.
+			//$scope.list = data.data;
+			//	updating list.page value.
+			$scope.list.page = data.data.page;
+			//	concatinating results from second page onto the first pages results.
+			$scope.list.results = $scope.list.results.concat(data.data.results);
+		});
+	}
 });
 
 controllers.controller('MovieCtrl', function ($routeParams, movies, $scope) {
--- js/services.js
+++ js/services.js
@@ -4,16 +4,20 @@ var services = angular.module('movie.services', []);
 
 services.factory('movies', function( $http ){
 
-	var apiKey = 'API KEY GOES HERE';
 
 	function load(path, params) {
-		return $http.jsonp('https://api.themoviedb.org/3/movie/' + path + '?api_key=' + apiKey + '&append_to_response=images,releases,trailers&language=en&include_image_language=en,null&callback=JSON_CALLBACK');
-
+		params = params || {};
+	    params.api_key = 'API KEY HERE';
+		params.callback = "JSON_CALLBACK";
+		params.append_to_response = "images,trailers";
+		params.language = "en";
+		params.include_image_language = "en,null";
+		return $http.jsonp('https://api.themoviedb.org/3/movie/' + path, { params: params });
 	}
 
 	return {
-		list: function(type) {
-			return load(type);
+		list: function(type, params) {
+			return load(type, params);
 		},
 		movie: function(type) {
 			return load(type);
